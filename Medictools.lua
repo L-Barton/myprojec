3c90d2c49a1894af5174d85434442e58f913bb07
Misanthrope123 <52921136+Misanthrope123@users.noreply.github.com>
2019-07-29 13:35:40 +0300

    Add files via upload

--- Medictools.lua
+++ Medictools.lua
@@ -2476,7 +2476,7 @@ end
 
 function update()
 	local fpath = os.getenv('TEMP') .. '\\update.json'
-	downloadUrlToFile('https://raw.githubusercontent.com/Misanthrope123/myprojec/master/update.json', fpath, function(id, status, p1, p2)
+	downloadUrlToFile('https://raw.githubusercontent.com/Misanthrope123/myprojec/master/updatemedic.json', fpath, function(id, status, p1, p2)
 		if status == dlstatus.STATUS_ENDDOWNLOADDATA then
             local f = io.open(fpath, 'r')
             if f then
